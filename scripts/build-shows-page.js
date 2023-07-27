bcf1c0e05c535486ac058f93783e24b3d4abc659
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-07-27 02:48:44 -0400

    implementing shows functionality

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// Selecting elements
+
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+const footer = document.querySelector(".footer");
+
+// Creating shows section
+
+const showsSection = document.createElement("section");
+showsSection.className = "section__shows";
+hero.append(showsSection);
+
+const tourSectionContainer = document.createElement("div");
+tourSectionContainer.className = "section__shows--tours";
+showsSection.appendChild(tourSectionContainer);
+
+const showsHeader = document.createElement("h1");
+showsHeader.className = "section__shows--header";
+showsHeader.textContent = "Please view tour dates:";
+tourSectionContainer.appendChild(showsHeader);
+
+// Creating tours
+
+const tours = [
+  {
+    date: "7.26.2023",
+    city: "Detroit, Michigan",
+    time: "8:00PM",
+  },
+  {
+    date: "7.27.2023",
+    city: "New York, New York",
+    time: "9:00PM",
+  },
+  {
+    date: "7.28.2023",
+    city: "Florida, Miami",
+    time: "10:00PM",
+  },
+];
+
+tours.forEach((tour) => {
+  const tourContainer = document.createElement("div");
+  tourContainer.className = "tour__container";
+  tourContainer.style.backgroundColor = "#323232";
+  tourContainer.style.margin = "0rem 2rem 2rem 2rem";
+  tourContainer.style.color = "#FAFAFA";
+  tourContainer.style.padding = "1rem";
+  tourContainer.style.borderRadius = "5px";
+  tourSectionContainer.appendChild(tourContainer);
+
+  const tourDate = document.createElement("p");
+  tourDate.className = "tour__container--date";
+  tourDate.textContent = tour.date;
+  tourDate.style.fontSize = "1.5rem";
+  tourContainer.appendChild(tourDate);
+
+  const tourCity = document.createElement("p");
+  tourCity.className = "tour__container--city";
+  tourCity.textContent = tour.city;
+  tourContainer.appendChild(tourCity);
+
+  const tourTime = document.createElement("p");
+  tourTime.className = "tour__container--time";
+  tourTime.textContent = tour.time;
+  tourContainer.appendChild(tourTime);
+
+  tourContainer.insertAdjacentHTML("afterbegin", tourDate, tourCity, tourTime);
+});
